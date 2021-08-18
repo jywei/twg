a912dd3c9b27fdebebaa61918d87ce0c247c0995
Roy Wei <roywjy@gmail.com>
2021-08-18 16:03:07 +1000

    Add underscore.go

---
+++ underscore/underscore_test.go
@@ -0,0 +1,12 @@
+package underscore
+
+import "testing"
+
+func TestCamel(t *testing.T) {
+	arg := "thisIsACamelCaseString"
+	want := "this_is_a_camel_case_string"
+	got := Camel(arg)
+	if got != want {
+		t.Errorf("Camel(%q) = %q; want %q", arg, got, want)
+	}
+}
