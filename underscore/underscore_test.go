3ad09ba3dd74e056e8c719542f7ad7cb4d9e457b
Roy Wei <roywjy@gmail.com>
2021-08-18 16:37:34 +1000

    Use table driven tests to add more tests

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -3,10 +3,19 @@ package underscore
 import "testing"
 
 func TestCamel(t *testing.T) {
-	arg := "thisIsACamelCaseString"
-	want := "this_is_a_camel_case_string"
-	got := Camel(arg)
-	if got != want {
-		t.Errorf("Camel(%q) = %q; want %q", arg, got, want)
+	testCases := []struct {
+		arg  string
+		want string
+	}{
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
 	}
+	for _, tc := range testCases {
+		got := Camel(tc.arg)
+		if got != tc.want {
+			t.Errorf("Camel(%q) = %q; want %q", tc.arg, got, tc.want)
+		}
+	}
+
 }
