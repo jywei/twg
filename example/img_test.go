bb6cd716d4a4bb28189c201302dd0f515014686a
Roy Wei <roywjy@gmail.com>
2021-08-18 15:15:38 +1000

    Use example_crop_test instead

--- example/img_test.go
+++ example/img_test.go
@@ -1,34 +1,8 @@
 package example
 
 import (
-	"fmt"
-	"io"
 	"testing"
 )
-
-func Example_crop() {
-	var r io.Reader
-	img, err := Decode(r)
-	if err != nil {
-		panic(err)
-	}
-	err = Crop(img, 0, 0, 20, 20)
-	if err != nil {
-		panic(err)
-	}
-	var w io.Writer
-	if err != nil {
-		panic(err)
-	}
-	err = Encode(img, w)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("See out.jpg for the cropped image.")
-
-	// Output:
-	// See out.jpg for the cropped image.
-}
 
 func TestDecode(t *testing.T) {
 
